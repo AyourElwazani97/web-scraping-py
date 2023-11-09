143efd1e78abf8ce49ee68b4cee0e804d9257f73
ayoubwazane <ayoubwazane306@gmail.com>
2023-11-09 01:02:48 +0100

    installing selenium to get data from websites that uses js frameworks

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import csv
+
+driver = webdriver.Chrome()
+driver.get("https://www.iris.ma/")
+
+html = driver.page_source
+soup = BeautifulSoup(html, "lxml")
+
+title = soup.find_all("div", {"class", "product-description"})
+print(title)
